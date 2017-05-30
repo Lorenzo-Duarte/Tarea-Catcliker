cd2adc33282695771d5a0aece6204baffc6a0e6a
Nestor Ferreira <nestordavid1009@hotmail.com>
2017-05-30 15:32:27 -0400

    lorenz.duart@gmail.com

---
+++ js/app.js
@@ -0,0 +1,47 @@
+/*
+*dato el proyecto catclicker declarar una variable cat 
+con los atributos name, photo, voteup, votedown
+asignar los valores del modelo datos a la estructura 
+html e implementar los eventos clik para el incremento 
+o decremento de los votos
+*/
+
+var cat = {
+	name : 'Michi',
+	photo : 'img/cats/1.jpg',
+	voteup:10,
+	votedown:11
+}
+
+
+function init(){
+	showData(cat);
+	bindEvent();
+}
+
+function showData(cat){
+
+	$('h1').text(cat.name);
+	$('img').attr('src',cat.photo);
+	$('#up span').text(cat.voteup);
+	$('#down span').text(cat.votedown);
+}
+
+
+function clickUp(){
+	cat.voteup++;
+	$('#up span').text(cat.voteup);
+}
+
+function clickDown(){
+	cat.votedown++;
+	$('#down span').text(cat.votedown);
+}
+
+
+function bindEvent(){
+	$('#up').click(clickUp);
+	$('#down').click(clickDown);	
+}
+
+init();
